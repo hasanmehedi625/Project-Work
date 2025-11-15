e210fa4096b24fd3472cbd4e76cdf429f241697b
Md. Mehedi Hasan <mdmehedi.hasan@wsu.edu>
2025-11-15 12:07:11 -0800

    Add files via upload

--- OpenHDF5.py
+++ OpenHDF5.py
@@ -1,54 +1,60 @@
-import gdal, os
+from osgeo import gdal
+import os
 
-## List input raster files
-os.chdir('C:\\InputFolder')
+## CHANGE THIS TO YOUR FOLDER WITH .h5 FILES
+os.chdir('C:/WSU Journey/Weekly Report to Prof. Lee/Black Marble Experiment')
 rasterFiles = os.listdir(os.getcwd())
-#print(rasterFiles)
 
-#Get File Name Prefix
-rasterFilePre = rasterFiles[0][:-3]
-print(rasterFilePre)
+# Filter only .h5 files
+rasterFiles = [f for f in rasterFiles if f.endswith('.h5')]
 
-fileExtension = "_BBOX.tif"
-
-## Open HDF file
-hdflayer = gdal.Open(rasterFiles[0], gdal.GA_ReadOnly)
-
-#print (hdflayer.GetSubDatasets())
-
-# Open raster layer
-#hdflayer.GetSubDatasets()[0][0] - for first layer
-#hdflayer.GetSubDatasets()[1][0] - for second layer ...etc
-subhdflayer = hdflayer.GetSubDatasets()[0][0]
-rlayer = gdal.Open(subhdflayer, gdal.GA_ReadOnly)
-#outputName = rlayer.GetMetadata_Dict()['long_name']
-
-#Subset the Long Name
-outputName = subhdflayer[92:]
-
-outputNameNoSpace = outputName.strip().replace(" ","_").replace("/","_")
-outputNameFinal = outputNameNoSpace + rasterFilePre + fileExtension
-print(outputNameFinal)
-
-outputFolder = "C:\\OutputFolder\\"
-
-outputRaster = outputFolder + outputNameFinal
-
-#collect bounding box coordinates
-HorizontalTileNumber = int(rlayer.GetMetadata_Dict()["HorizontalTileNumber"])
-VerticalTileNumber = int(rlayer.GetMetadata_Dict()["VerticalTileNumber"])
+if not rasterFiles:
+    print("No H5 files found in folder")
+else:
+    # Get File Name Prefix
+    rasterFilePre = rasterFiles[0][:-3]
+    print(rasterFilePre)
     
-WestBoundCoord = (10*HorizontalTileNumber) - 180
-NorthBoundCoord = 90-(10*VerticalTileNumber)
-EastBoundCoord = WestBoundCoord + 10
-SouthBoundCoord = NorthBoundCoord - 10
-
-EPSG = "-a_srs EPSG:4326" #WGS84
-
-translateOptionText = EPSG+" -a_ullr " + str(WestBoundCoord) + " " + str(NorthBoundCoord) + " " + str(EastBoundCoord) + " " + str(SouthBoundCoord)
-
-translateoptions = gdal.TranslateOptions(gdal.ParseCommandLine(translateOptionText))
-gdal.Translate(outputRaster,rlayer, options=translateoptions)
-
-#Display image in QGIS (run it within QGIS python Console) - remove comment to display
-#iface.addRasterLayer(outputRaster, outputNameFinal)
+    fileExtension = "_BBOX.tif"
+    
+    ## Open HDF file
+    hdflayer = gdal.Open(rasterFiles[0], gdal.GA_ReadOnly)
+    
+    # Open raster layer (first subdataset)
+    subhdflayer = hdflayer.GetSubDatasets()[0][0]
+    rlayer = gdal.Open(subhdflayer, gdal.GA_ReadOnly)
+    
+    # Subset the Long Name
+    outputName = subhdflayer[92:]
+    
+    outputNameNoSpace = outputName.strip().replace(" ","_").replace("/","_")
+    outputNameFinal = outputNameNoSpace + rasterFilePre + fileExtension
+    print(outputNameFinal)
+    
+    ## CHANGE THIS TO YOUR OUTPUT FOLDER
+    outputFolder = "C:/WSU Journey/Weekly Report to Prof. Lee/Black Marble Experiment"
+    
+    outputRaster = outputFolder + outputNameFinal
+    
+    # Collect bounding box coordinates
+    HorizontalTileNumber = int(rlayer.GetMetadata_Dict()["HorizontalTileNumber"])
+    VerticalTileNumber = int(rlayer.GetMetadata_Dict()["VerticalTileNumber"])
+        
+    WestBoundCoord = (10*HorizontalTileNumber) - 180
+    NorthBoundCoord = 90-(10*VerticalTileNumber)
+    EastBoundCoord = WestBoundCoord + 10
+    SouthBoundCoord = NorthBoundCoord - 10
+    
+    EPSG = "-a_srs EPSG:4326"
+    
+    translateOptionText = EPSG+" -a_ullr " + str(WestBoundCoord) + " " + str(NorthBoundCoord) + " " + str(EastBoundCoord) + " " + str(SouthBoundCoord)
+    
+    translateoptions = gdal.TranslateOptions(gdal.ParseCommandLine(translateOptionText))
+    gdal.Translate(outputRaster, rlayer, options=translateoptions)
+    
+    print(f"Successfully created: {outputRaster}")
+    
+    # Display image in QGIS
+    iface.addRasterLayer(outputRaster, outputNameFinal)
+    
+    print("Done")
